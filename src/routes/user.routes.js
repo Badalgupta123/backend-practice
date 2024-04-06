203dde3d3f89790b09182d078e43b97f09658387
Badalgupta123 <badalgupta7898@gmail.com>
2024-04-06 12:00:43 +0530

    lec-20 routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from "../controllers/user.controller.js"
+import {
+    changeCurrentPassword,
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +34,15 @@ router.route("/login").post(loginUser)
 // secured routes - add middleware to check weather user is logged in or not
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails) // use patch method because
+    // we want to update the fields that has been updated not all so dont use post 
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar) // .single because we accept only single file
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default router
